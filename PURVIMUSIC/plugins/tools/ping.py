27d849c565fe96f38940afdb1ef546b26b0f7d11
Block2002 <marwin2005op@gmail.com>
2025-02-11 12:16:36 +0530

    Update ping.py

--- PURVIMUSIC/plugins/tools/ping.py
+++ PURVIMUSIC/plugins/tools/ping.py
@@ -9,18 +9,18 @@ from PURVIMUSIC.core.call import PURVI
 from PURVIMUSIC.utils import bot_sys_stats
 from PURVIMUSIC.utils.decorators.language import language
 from PURVIMUSIC.utils.inline import supp_markup
-from config import BANNED_USERS
+from config import BANNED_USERS, PING_IMG_URL
 
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await PURVI.ping()
+    pytgping = await Anony.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
